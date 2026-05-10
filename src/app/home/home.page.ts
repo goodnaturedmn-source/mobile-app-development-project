83bb1e5e65153f2c57f6191e9e956d65641e1ce5
Anastasiia Miroshnichenko <goodnatured.mn@gmail.com>
2026-05-10 04:17:00 -0400

    added tmdb api movie list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent
+  ]
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  movies: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    this.http
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${environment.apiKey}`)
+      .subscribe((response: any) => {
+        this.movies = response.results;
+      });
+  }
 }
